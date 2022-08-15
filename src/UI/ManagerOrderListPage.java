28c50b37f7e2afb0ca7fee5b46f5af4adafbbdfc
JaeHyuk <gu9062@naver.com>
2022-08-16 07:36:42 +0900

    UI 연결 및 구현 완료

--- src/UI/ManagerOrderListPage.java
+++ src/UI/ManagerOrderListPage.java
@@ -20,6 +20,7 @@ import java.awt.CardLayout;
 public class ManagerOrderListPage extends JFrame {
 
 	private JPanel contentPane;
+	private String branch;
 
 	/**
 	 * Launch the application.
@@ -42,10 +43,8 @@ public class ManagerOrderListPage extends JFrame {
 	 */
 	
 	public ManagerOrderListPage() {
-		System.out.println("Session out");
-	}
-	
-	public ManagerOrderListPage(String branch) {
+		branch = "919";
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1000,600);
 		setLocationRelativeTo(null);
@@ -93,37 +92,128 @@ public class ManagerOrderListPage extends JFrame {
 		ViewPanel.add(OutputPanel, BorderLayout.CENTER);
 		OutputPanel.setLayout(new CardLayout(0, 0));
 		
+		ManagerOrderListPanel OrderListPanel = new ManagerOrderListPanel(branch);
+		ManagerStockManagementPanel StockManagementPanel = new ManagerStockManagementPanel();
+		ManagerSalesStatusPanel SalesStatusPanel = new ManagerSalesStatusPanel();
+		
+		
+		OutputPanel.add("one",OrderListPanel/*, BorderLayout.CENTER*/);
+		OutputPanel.add("two", StockManagementPanel/* , BorderLayout.CENTER */);
+		OutputPanel.add("three", SalesStatusPanel/* , BorderLayout.CENTER */);
+		
+		
+		CardLayout cl = (CardLayout) OutputPanel.getLayout();
+		cl.first(OutputPanel);
+		
 		//주문 내역 버튼
 		OrderListBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ManagerOrderListPanel OrderListPanel = new ManagerOrderListPanel();
-				OutputPanel.add(OrderListPanel, BorderLayout.CENTER);
-				OrderListPanel.setVisible(true);
+				cl.show(OutputPanel, "one");
 			}
 		});
 		
 		//재고 관리 버튼
 		StockManagementBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ManagerStockManagementPanel StockManagementPanel = new ManagerStockManagementPanel();
-				OutputPanel.add(StockManagementPanel, BorderLayout.CENTER);
-				StockManagementPanel.setVisible(true);
+				cl.show(OutputPanel, "two");
 			}
 		});
 		
 		//매출 현황 버튼
 		SalesStatusBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ManagerSalesStatusPanel StockManagementPanel = new ManagerSalesStatusPanel();
-				OutputPanel.add(StockManagementPanel, BorderLayout.CENTER);
-				StockManagementPanel.setVisible(true);
+				cl.show(OutputPanel, "three");
 			}
 		});
 		
-		ManagerOrderListPanel OrderListPanel = new ManagerOrderListPanel();
-		OutputPanel.add(OrderListPanel, BorderLayout.CENTER);
-		System.out.println(OutputPanel.getSize());
 		
+		
+	}
+	
+	public ManagerOrderListPage(String branch) {
+		this.branch = branch;
+		System.out.println(branch);
+		
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1000,600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Menu");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("뒤로가기");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("종료");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel ViewPanel = new JPanel();
+		contentPane.add(ViewPanel, BorderLayout.CENTER);
+		ViewPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel MenuPanel = new JPanel();
+		MenuPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		ViewPanel.add(MenuPanel, BorderLayout.WEST);
+		MenuPanel.setLayout(new BoxLayout(MenuPanel, BoxLayout.Y_AXIS));
+		
+		JButton OrderListBtn = new JButton("주문 내역");
+		
+		MenuPanel.add(OrderListBtn);
+		
+		JButton StockManagementBtn = new JButton("재고 관리");
+		
+		MenuPanel.add(StockManagementBtn);
+		
+		JButton SalesStatusBtn = new JButton("매출 현황");
+		
+		MenuPanel.add(SalesStatusBtn);
+		
+		JPanel OutputPanel = new JPanel();
+		OutputPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		ViewPanel.add(OutputPanel, BorderLayout.CENTER);
+		OutputPanel.setLayout(new CardLayout(0, 0));
+		
+		ManagerOrderListPanel OrderListPanel = new ManagerOrderListPanel(branch);
+		ManagerStockManagementPanel StockManagementPanel = new ManagerStockManagementPanel();
+		ManagerSalesStatusPanel SalesStatusPanel = new ManagerSalesStatusPanel();
+		
+		OutputPanel.add("one",OrderListPanel/*, BorderLayout.CENTER*/);
+		OutputPanel.add("two", StockManagementPanel/* , BorderLayout.CENTER */);
+		OutputPanel.add("three", SalesStatusPanel/* , BorderLayout.CENTER */);
+		
+		CardLayout cl = (CardLayout) OutputPanel.getLayout();
+		cl.first(OutputPanel);
+		
+		//주문 내역 버튼
+		OrderListBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(OutputPanel, "one");
+			}
+		});
+		
+		//재고 관리 버튼
+		StockManagementBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(OutputPanel, "two");
+			}
+		});
+		
+		//매출 현황 버튼
+		SalesStatusBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(OutputPanel, "three");
+			}
+		});
 	}
 
 }
--- src/UI/ManagerOrderListPanel.java
+++ src/UI/ManagerOrderListPanel.java
@@ -22,7 +22,7 @@ import java.awt.event.ActionEvent;
 
 public class ManagerOrderListPanel extends JPanel {
 	private JTable table;
-	String branch;
+	private String branch;
     String orderNum;
 	private ArrayList<Orderlist> list = new ArrayList<>();
 	String header[] = {"주문번호", "주문내역", "금액"};
@@ -125,6 +125,101 @@ public class ManagerOrderListPanel extends JPanel {
 
 	}
 	
+	public ManagerOrderListPanel(String branch) {
+		
+		JPanel BorderNorthPanel = new JPanel();
+		JPanel BorderNorthDetailPanel = new JPanel();
+		JPanel BorderCenterPanel = new JPanel();
+		JPanel BorderCenterPanel1 = new JPanel();
+		JPanel BorderCenterPanel2 = new JPanel();
+		
+		this.branch = branch;
+		
+		//최상위 패널
+		setLayout(new BorderLayout(0, 0));
+		//
+		
+		//Border Center 최상위 패널
+		FlowLayout fl = new FlowLayout();
+		BorderCenterPanel.setLayout(fl);
+		this.add(BorderCenterPanel, BorderLayout.CENTER);
+		//
+		
+		BorderCenterPanel.add(BorderCenterPanel1);
+		BorderCenterPanel.add(BorderCenterPanel2);
+		
+		//라벨
+		JLabel lblNewLabel = new JLabel("실시간 주문 내역");
+		JLabel OrderMenuLabel = new JLabel("현재 주문 현황");
+		JLabel CompleteMenuLabel = new JLabel("완료된 주문");
+		
+		lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 14));
+		OrderMenuLabel.setFont(new Font("굴림", Font.PLAIN, 12));
+		CompleteMenuLabel.setFont(new Font("굴림", Font.PLAIN, 12));
+		
+		lblNewLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
+		lblNewLabel.setHorizontalAlignment(JLabel.CENTER);
+		OrderMenuLabel.setHorizontalAlignment(JLabel.CENTER);
+		CompleteMenuLabel.setHorizontalAlignment(JLabel.CENTER);
+		//
+		
+		//Border North 최상위 패널
+		BorderNorthPanel.setLayout(new BoxLayout(BorderNorthPanel, BoxLayout.Y_AXIS));
+		add(BorderNorthPanel, BorderLayout.NORTH);
+		
+		BorderNorthPanel.add(lblNewLabel);
+		//
+		
+		FlowLayout fl1 = new FlowLayout();
+		BorderNorthDetailPanel.setLayout(fl1);
+		
+		fl1.setHgap(350);
+		
+		BorderNorthPanel.add(BorderNorthDetailPanel);
+		
+		BorderNorthDetailPanel.add(OrderMenuLabel);
+		BorderNorthDetailPanel.add(CompleteMenuLabel);
+		
+		
+		JTable table = new JTable(createTableModel1());
+		JTable table2 = new JTable(createTableModel2());
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+		scrollPane.setPreferredSize(new Dimension(400, 125));
+		JScrollPane scrollPane2 = new JScrollPane(table2);
+		table2.setFillsViewportHeight(true);
+		scrollPane2.setPreferredSize(new Dimension(400, 125));
+		JButton CompeteMenuBtn = new JButton("메뉴준비 완료");
+		
+		//주문완료 버튼 클릭
+		CompeteMenuBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				int line = table.getSelectedRow();
+				
+				int selectOrderNum = (int)table.getValueAt(line, 0);
+				
+				(new OrderlistDAO()).completeOrder(selectOrderNum);
+				
+				DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+				tableModel.setRowCount(0);
+				DefaultTableModel tableModel2 = (DefaultTableModel) table2.getModel();
+				tableModel2.setRowCount(0);
+			
+				table.setModel(createTableModel1());
+				table2.setModel(createTableModel2());
+			}
+		});
+		
+		this.add(CompeteMenuBtn, BorderLayout.SOUTH);
+		BorderCenterPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		
+		BorderCenterPanel1.add(scrollPane);
+		BorderCenterPanel2.add(scrollPane2);
+
+	}
+	
 	
 	public DefaultTableModel createTableModel1(){
 		
@@ -136,9 +231,6 @@ public class ManagerOrderListPanel extends JPanel {
 			    }
 		};
 		
-		//나중에 바꾸기
-        branch = "919";
-        
 		//model = (DefaultTableModel) table.getModel();
         list = (new OrderlistDAO()).getList(branch);
         for(int i = 0;i<list.size();i++){
@@ -160,9 +252,6 @@ public class ManagerOrderListPanel extends JPanel {
 			       return false;
 			    }
 		};
-		
-		//나중에 바꾸기
-        branch = "919";
         
 		//model2 = (DefaultTableModel) table.getModel();
         list = (new OrderlistDAO()).getCompleteList(branch);
